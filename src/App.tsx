0ef69d5326556449fadcace6e41fdb505b7a0357
Haouri <saadhaouri@gmail.com>
2023-11-15 17:51:48 +0100

    the changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,19 @@
-import { useState } from "react";
-
 import "./App.css";
-export interface isSignUp {
-  email: string;
-  password: string;
-}
+import Footer from "./Component/FooterDiv/Footer";
+import Job from "./Component/JobDiv/Job";
+import NavBar from "./Component/NavBar/NavBar";
+import Search from "./Component/SearchDiv/Search";
+import Value from "./Component/ValueDiv/Value";
 
 function App() {
-  const [formData, setFormData] = useState<isSignUp>({
-    email: "",
-    password: "",
-  });
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
   return (
-    <>
-      <input
-        type="text"
-        name="email"
-        value={formData.email}
-        onChange={handleInputChange}
-        placeholder="enter your first name "
-      />
-      <br />
-      <input
-        type="password"
-        name="password"
-        value={formData.password}
-        placeholder="enter your last name "
-        onChange={handleInputChange}
-      />
-      <br />
-      <button> Submit </button>
-      <div>
-        <p> Email : {formData.email}</p>
-        <p>
-          Password:{" "}
-          <span>
-            {Array.from({ length: formData.password.length }, (_, index) => (
-              <span key={index}>*</span>
-            ))}
-          </span>
-        </p>
-      </div>
-    </>
+    <div className=" m-auto ">
+      <NavBar />
+      <Search />
+      <Job />
+      <Value />
+      <Footer />
+    </div>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'blueColor': "#2a68ff"
+        ,'geyIsh' : '#f1f4f8',
+      },
+    },
+  },
+  plugins: [],
+};
